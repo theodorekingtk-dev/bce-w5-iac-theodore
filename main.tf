72fca4d28feaf1184478d1155a90c903d93fed3d
theodorekingtk-dev <theodoreking.tk@gmail.com>
2026-06-04 10:15:55 -0500

    Update Terraform required version and AWS region

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.5.0"
 
   required_providers {
     aws = {
@@ -10,9 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
-}
-
-resource "aws_s3_bucket" "terraform_lab" {
-  bucket = "theodore-terraform-lab-2026"
+  region = var.aws_region
 }
