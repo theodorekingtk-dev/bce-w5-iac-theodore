f2144adee156447776748b00956ad5654a3eea98
theodorekingtk-dev <theodoreking.tk@gmail.com>
2026-06-04 09:46:37 -0500

    Add multiple variables for AWS configuration

--- variables.tf
+++ variables.tf
@@ -1,3 +1,35 @@
 variable "aws_region" {
   default = "us-east-2"
 }
+
+variable "project_name" {
+  default = "workforceconnect"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_2_cidr" {
+  default = "10.0.3.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "db_name" {
+  default = "workforceconnect"
+}
+
+variable "db_username" {
+  default = "admin"
+}
