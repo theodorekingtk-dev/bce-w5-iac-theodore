046cadd904e0b4eba0037ec86535a9b60109a1b2
theodorekingtk-dev <theodoreking.tk@gmail.com>
2026-06-04 01:59:40 -0500

    Initialize Terraform configuration for AWS S3 bucket

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_lab" {
+  bucket = "theodore-terraform-lab-2026"
+}
