5b049f821abec3dcd889ba0b80f86ee27aec6658
theodorekingtk-dev <theodoreking.tk@gmail.com>
2026-06-04 10:00:31 -0500

    Add AWS RDS MySQL instance and subnet group

---
+++ database.tf
@@ -0,0 +1,29 @@
+resource "aws_db_subnet_group" "main" {
+  name = "${var.project_name}-db-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private1.id,
+    aws_subnet.private2.id
+  ]
+
+  tags = {
+    Name = "${var.project_name}-db-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  identifier             = "${var.project_name}-mysql"
+  allocated_storage      = 20
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = "Password123!"
+  skip_final_snapshot    = true
+
+  db_subnet_group_name   = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+
+  publicly_accessible = false
+}
