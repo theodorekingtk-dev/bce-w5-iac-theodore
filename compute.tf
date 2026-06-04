f30a6db27b471427a059955b974bba0700352404
theodorekingtk-dev <theodoreking.tk@gmail.com>
2026-06-04 10:23:21 -0500

    Fix EC2 resource configuration

--- compute.tf
+++ compute.tf
@@ -9,11 +9,13 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = var.instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
     Name = "${var.project_name}-webserver"
